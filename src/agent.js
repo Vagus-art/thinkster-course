a76e29b602a24ded5cbf746653407fa1caf34238
Vagus-art <agustin.favo@gmail.com>
2020-01-17 23:28:35 -0300

    changed superagent to fetch api

--- src/agent.js
+++ src/agent.js
@@ -1,15 +1,10 @@
-import superagentPromise from 'superagent-promise';
-import _superagent from 'superagent';
-
-const superagent = superagentPromise(_superagent, global.Promise);
-
 const API_ROOT = 'https://conduit.productionready.io/api';
 
-const responseBody = res => res.body;
+const responseBody = res => res.json();
 
 const requests = {
     get:url =>
-        superagent.get(`${API_ROOT}${url}`).then(responseBody)
+        fetch(`${API_ROOT}${url}`).then(responseBody)
 };
 
 const Articles = {
